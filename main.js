d9495077e72cfff714cba0a8ae0fc3eff52b61ec
Dago Casiva <casivadago@gmail.com>
2023-10-26 23:13:43 -0300

    3ra preentrega

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-let nombre = prompt("Ingrese su nombre completo")
-    alert("Bienvenido " + nombre)
+// // Bienvenida
+// let nombre = prompt("Ingrese su nombre completo")
+//     alert("Bienvenido " + nombre)
+// // Bucle
+// alert("Cuenta hasta 3 para empezar a comprar")
+// for (let i = 1; i<=3; i++){
+//     alert(i)
+// }
 
-alert("Cuenta hasta 3 para empezar a comprar")
-for (let i = 1; i<=3; i++){
-    alert(i)
-}
 
+// Funcion Constructora
 const Prod = function(codigo, descripcion, precio){
     this.codigo = codigo
     this.descripcion = descripcion
@@ -20,78 +23,98 @@ let prod5 = new Prod (5,"Mate Personalizado", 10000)
 
 let catalogo = [prod1, prod2, prod3, prod4, prod5]
 
-function filtro(){
-    let busqueda = prompt("Ingresa lo que estás buscando (Ver en consola)").trim().toUpperCase()
-    let filtrado = catalogo.filter((prod)=>prod.descripcion.toUpperCase().includes(busqueda))
-    if (filtrado.length > 0){
-        console.table(filtrado)
-    } else {
-        alert("Lo sentimos, no se encontraron coincidencias")
+function mostrarProductos(productos){
+    let contenedor = document.querySelector(".saraza")
+    contenedor.innerHTML = ""
+    if(productos.length === 0){
+        contenedor.innerHTML = "<p>No se encontraron coincidencias</p>"
+    } else{
+        const lista = document.createElement("ul")
+        productos.forEach((prod)=>{
+            const li = document.createElement("li")
+            li.innerHTML = `<h3> Codigo: ${prod.codigo}</h3>
+                            <p>  Producto: ${prod.descripcion}</p>
+                            <b> $ ${prod.precio}</b>`;
+            lista.appendChild(li)
+        })
+        contenedor.appendChild(lista)
+        localStorage.setItem("resultadosBusqueda", JSON.stringify(productos));
+        }
     }
-}
 
-const localidades = [
-    {provincia: "cordoba", envio: 1500},
-    {provincia: "catamarca", envio: 3500},
-    {provincia: "bsas", envio: 3000}
-]
-
-const pedirValor = (peticion) => {
-    let txt = prompt(peticion).trim().toLowerCase()
-    return txt
-}
-
-const valorPrecio = () => {
-    let codigoIngresado = parseInt( pedirValor("Ingrese el codigo de su producto") )
-    let codigosDeCatalogo = catalogo.map( (producto) => producto.codigo )
-    let loIncluye = codigosDeCatalogo.includes(codigoIngresado)
-
-    if (loIncluye) {
-        let prod = catalogo.find( (producto) => producto.codigo == codigoIngresado )
-        return prod.precio
-    } else {
-        return 0
-    }
-} 
-const valorEnvio = () => {
-    let prov = pedirValor("Ingrese su provincia para realizar el envio")
-    let provincias = localidades.map( (localidad) => localidad.provincia)
-    let laIncluye = provincias.includes(prov)
-
-    if (laIncluye) {
-        let lugar = localidades.find( (localidad) => localidad.provincia == prov )
-        return lugar.envio
-    } else {
-        return 0
-    }
-}
-
-
-const comprar = () => {
-    let subtotal = valorPrecio()
-
-    if (subtotal) {
-        let cantidad = parseInt(pedirValor("Ingrese la cantidad que desea comprar"))
-        let envio = valorEnvio()
-        // condicion ? true : false
-        envio ? alert("su coste total es de " + (subtotal * cantidad + envio)) : alert("No tenemos envío disponible para la localidad ingresada")
-
-    } else {
-        alert("El codigo ingresado del producto que buscas no existe")
-    }
-}
-
-
-
-
-const btn = document.querySelector(".boton")
-btn.addEventListener('click', (e) => {
+let form = document.querySelector("#formulario")
+form.addEventListener("submit", function (e){
     e.preventDefault()
-    comprar()
-})
+    let valor = document.querySelector("#text").value.toString();
+    busqueda = valor.trim().toUpperCase()
+    const filtrado = catalogo.filter((prod)=>prod.descripcion.toUpperCase().includes(busqueda))
+    mostrarProductos(filtrado)
+}) 
 
-const filt = document.querySelector(".filtro")
-filt.addEventListener('click', (e) => {
-    e.preventDefault()
-    filtro()
-})
+const resultadosGuardados = localStorage.getItem("resultadosBusqueda");
+if (resultadosGuardados) {
+const resultadosParseados = JSON.parse(resultadosGuardados);
+mostrarProductos(resultadosParseados);
+}
+
+
+
+
+
+// SEGUNDA PREENTREGA
+
+// const localidades = [
+//     {provincia: "cordoba", envio: 1500},
+//     {provincia: "catamarca", envio: 3500},
+//     {provincia: "bsas", envio: 3000}
+// ]
+
+
+// const valorPrecio = () => {
+//     let inputCodigo = document.querySelector(".codigo")
+//     const inputNumero = inputCodigo.value
+//     const codigoIngresado = parseInt(inputNumero)
+//     console.log(codigoIngresado)
+//     let codigosDeCatalogo = catalogo.map( (producto) => producto.codigo )
+//     let loIncluye = codigosDeCatalogo.includes(codigoIngresado)
+
+//     if (loIncluye) {
+//         let prod = catalogo.find( (producto) => producto.codigo == codigoIngresado )
+//         return prod.precio
+//     } else {
+//         return 0
+//     }
+// }
+
+// const valorEnvio = () => {
+//     let prov = document.querySelector(".lugar").value.toString()
+//     let provincias = localidades.map( (localidad) => localidad.provincia)
+//     let laIncluye = provincias.includes(prov)
+
+//     if (laIncluye) {
+//         let lugar = localidades.find( (localidad) => localidad.provincia == prov )
+//         return lugar.envio
+//     } else {
+//         return 0
+//     }
+// }
+
+// const comprar = () => {
+//     let subtotal = valorPrecio()
+
+//     if (subtotal) {
+//         let cantidad = document.querySelector(".cant")
+//         let envio = valorEnvio()
+//         resultado = subtotal + envio * cantidad
+//         return resultado
+//     } else {
+//         alert("El codigo ingresado del producto que buscas no existe")
+//     }
+// // }
+
+
+// // let formCompra = document.querySelector("#compra")
+// // formCompra.addEventListener("submit", function (e){
+// //     e.preventDefault()
+// //     comprar()
+// // })
