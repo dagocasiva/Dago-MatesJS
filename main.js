81e34615c3753a742e3b8d70121de236b2c2f25b
Dago Casiva <casivadago@gmail.com>
2023-11-10 17:21:40 -0300

    librerias, json y promesas

--- main.js
+++ main.js
@@ -1,46 +1,197 @@
-// // Bienvenida
-// let nombre = prompt("Ingrese su nombre completo")
-//     alert("Bienvenido " + nombre)
-// // Bucle
-// alert("Cuenta hasta 3 para empezar a comprar")
-// for (let i = 1; i<=3; i++){
-//     alert(i)
-// }
+
+(async () => {
+    const { value: email } = await Swal.fire({
+    title: "Por favor, ingrese su email",
+    input: "email",
+    inputLabel: "Tu email es:",
+    inputPlaceholder: "ejemplo@mail.com"
+    });
+    if (email) {
+    Swal.fire(`Email ingresado: ${email}`);
+    }
+})()
 
 
-// Funcion Constructora
-const Prod = function(codigo, descripcion, precio){
-    this.codigo = codigo
+const Prod = function(img, id, descripcion, precio){
+    this.img = img
+    this.id = id
     this.descripcion = descripcion
     this.precio = precio
 }
 
-let prod1 = new Prod (1,"Termo Stanley", 15000)
-let prod2 = new Prod (2,"Termo Acero Inox", 7000)
-let prod3 = new Prod (3,"Mate Imperial", 10000)
-let prod4 = new Prod (4,"Mate Camionero", 6000)
-let prod5 = new Prod (5,"Mate Personalizado", 10000)
+// let prod1 = new Prod ("./img/termo-stanley.jpg", "termo-1","Termo Stanley", 15000)
+// let prod2 = new Prod ("./img/termo-acero.jpg", "termo-2", "Termo Acero Inox", 7000)
+// let prod3 = new Prod ("./img/mate-imperial.png", "mate-1","Mate Imperial", 10000)
+// let prod4 = new Prod ("./img/mate-camionero.jpg", "mate-2", "Mate Camionero", 6000)
+// let prod5 = new Prod ("./img/mate-personalizado.webp", "mate-3", "Mate Personalizado", 10000)
 
-let catalogo = [prod1, prod2, prod3, prod4, prod5]
+let catalogo = []
+
+fetch('productos.json')
+    .then(response => response.json())
+    .then(data => {
+    catalogo = data.map(prod => new Prod(prod.img, prod.id, prod.descripcion, prod.precio));
+    console.log(catalogo);
+    mostrarProductos(catalogo);
+    })
+    .catch(error => console.error('Error al cargar los productos:', error));
+
+
+const contenedor = document.querySelector(".productos")
+let botonesAgregar = document.querySelectorAll(".agregarProd")
+let botonesEliminar = document.querySelectorAll(".eliminar")
+const numero = document.querySelector(".numero")
+const total = document.querySelector(".total")
 
 function mostrarProductos(productos){
-    let contenedor = document.querySelector(".saraza")
+    console.log('Mostrando productos:', productos);
     contenedor.innerHTML = ""
     if(productos.length === 0){
-        contenedor.innerHTML = "<p>No se encontraron coincidencias</p>"
+        contenedor.innerHTML = '<p class="contenedorVacio">No se encontraron coincidencias</p>'
     } else{
-        const lista = document.createElement("ul")
         productos.forEach((prod)=>{
-            const li = document.createElement("li")
-            li.innerHTML = `<h3> Codigo: ${prod.codigo}</h3>
-                            <p>  Producto: ${prod.descripcion}</p>
-                            <b> $ ${prod.precio}</b>`;
-            lista.appendChild(li)
-        })
-        contenedor.appendChild(lista)
-        localStorage.setItem("resultadosBusqueda", JSON.stringify(productos));
+            const div = document.createElement("div");
+            div.classList.add("cartaProd");
+
+            div.innerHTML = `
+            <img class = "imagenProd" src ="${prod.img}" alt= "${prod.descripcion}"
+            <div class = "detallesProd">
+                <h3 class="tituloProd">${prod.descripcion}</h3>
+                <p class="precioProd">$${prod.precio}</p>
+                <button class="agregarProd" id="${prod.id}">Agregar al carrito</button>
+            </div>
+            `
+            contenedor.appendChild(div);
+        },
+        );
         }
+        actualizarBotonesAgregar()
     }
+
+
+function actualizarBotonesAgregar(){
+    botonesAgregar = document.querySelectorAll(".agregarProd");
+
+    botonesAgregar.forEach(boton => {
+        boton.addEventListener("click",agregar);
+    })
+}
+
+function actualizarBotonesEliminar(){
+    botonesEliminar = document.querySelectorAll(".eliminar");
+
+    botonesEliminar.forEach(boton => {
+        boton.addEventListener("click",eliminar);
+    })
+}
+
+const productosCarrito = [];
+
+
+function agregar(e){
+    const idBoton = e.currentTarget.id
+    const agregado = catalogo.find(prod => prod.id === idBoton)
+    
+    if(productosCarrito.some(prod => prod.id === idBoton)){
+        const index = productosCarrito.findIndex(prod => prod.id === idBoton);
+        productosCarrito[index].cantidad++
+    } else {
+        agregado.cantidad = 1
+        productosCarrito.push(agregado)
+    }
+    actualizarNumero()
+    mostrarCarrito()
+}
+
+
+
+function eliminar(e){
+    const idBoton = e.currentTarget.id
+    const index = productosCarrito.findIndex(prod => prod.id === idBoton);
+
+    productosCarrito.splice(index, 1)
+
+    mostrarCarrito()
+    actualizarNumero()
+}
+
+
+
+function actualizarNumero(){
+    let nuevoNumero = productosCarrito.length
+    numero.innerHTML = nuevoNumero
+}
+
+const carritoProductos = document.querySelector(".carritoProductos")
+
+
+
+function mostrarCarrito(){
+    carritoProductos.innerHTML = ""
+    if(productosCarrito.length === 0){
+        carritoProductos.innerHTML = `
+        <p>El carrito esta vacío</p>`
+    } else {
+        productosCarrito.forEach(prod => {
+
+        const divCarrito = document.createElement("div")
+        divCarrito.classList.add("prodCarrito")
+        divCarrito.innerHTML = `
+                <img class = "imagenProdCarrito" src ="${prod.img}" alt= "${prod.descripcion}"/>
+                <div class = "descripcionProdCarrito">
+                <h3 class="tituloProdCarrito">${prod.descripcion}</h3>
+                <div class = "cantidadProdCarrito">
+                    <p>Cantidad: ${prod.cantidad}</p>
+                </div>
+                <div class = "precioProdCarrito">
+                    <p>$${prod.precio}</p>
+                </div>
+                <div class="subtotal">
+                    <h3>Subtotal:</h3>
+                    <p>$${prod.precio * prod.cantidad}</p>
+                </div>
+                <button class="eliminar" id="${prod.id}">Eliminar</button>
+            `;
+            carritoProductos.appendChild(divCarrito)
+            });
+    }
+    actualizarBotonesEliminar()
+    actualizarTotal()
+} 
+
+function actualizarTotal(){
+    const resultadoTotal = productosCarrito.reduce((acc,prod) => acc + (prod.precio * prod.cantidad), 0)
+    total.innerHTML = 
+    `
+    <p>$${resultadoTotal}</p>
+    `
+}
+
+
+
+const botonComprar = document.querySelector(".botonComprar")
+
+botonComprar.addEventListener("click", comprar)
+function comprar(){
+    productosCarrito.length === 0 ? Swal.fire({
+        icon: "error",
+        title: "Lo sentimos, no pudimos realizar su compra",
+        text: "El carrito está vacío"
+    }) : 
+    
+    Swal.fire({
+        title: "Felicitaciones!",
+        text: "Su compra ha sido finalizada, te estaremos contactando.",
+        icon: "success"
+    });
+    productosCarrito.length = 0
+    mostrarCarrito()
+    actualizarNumero()
+}
+
+const localidades = document.querySelector(".localidades")
+
+
 
 let form = document.querySelector("#formulario")
 form.addEventListener("submit", function (e){
@@ -48,6 +199,7 @@ form.addEventListener("submit", function (e){
     let valor = document.querySelector("#text").value.toString();
     busqueda = valor.trim().toUpperCase()
     const filtrado = catalogo.filter((prod)=>prod.descripcion.toUpperCase().includes(busqueda))
+    localStorage.setItem("resultadosBusqueda", JSON.stringify(filtrado));
     mostrarProductos(filtrado)
 }) 
 
@@ -57,64 +209,5 @@ const resultadosParseados = JSON.parse(resultadosGuardados);
 mostrarProductos(resultadosParseados);
 }
 
+mostrarProductos(catalogo);
 
-
-
-
-// SEGUNDA PREENTREGA
-
-// const localidades = [
-//     {provincia: "cordoba", envio: 1500},
-//     {provincia: "catamarca", envio: 3500},
-//     {provincia: "bsas", envio: 3000}
-// ]
-
-
-// const valorPrecio = () => {
-//     let inputCodigo = document.querySelector(".codigo")
-//     const inputNumero = inputCodigo.value
-//     const codigoIngresado = parseInt(inputNumero)
-//     console.log(codigoIngresado)
-//     let codigosDeCatalogo = catalogo.map( (producto) => producto.codigo )
-//     let loIncluye = codigosDeCatalogo.includes(codigoIngresado)
-
-//     if (loIncluye) {
-//         let prod = catalogo.find( (producto) => producto.codigo == codigoIngresado )
-//         return prod.precio
-//     } else {
-//         return 0
-//     }
-// }
-
-// const valorEnvio = () => {
-//     let prov = document.querySelector(".lugar").value.toString()
-//     let provincias = localidades.map( (localidad) => localidad.provincia)
-//     let laIncluye = provincias.includes(prov)
-
-//     if (laIncluye) {
-//         let lugar = localidades.find( (localidad) => localidad.provincia == prov )
-//         return lugar.envio
-//     } else {
-//         return 0
-//     }
-// }
-
-// const comprar = () => {
-//     let subtotal = valorPrecio()
-
-//     if (subtotal) {
-//         let cantidad = document.querySelector(".cant")
-//         let envio = valorEnvio()
-//         resultado = subtotal + envio * cantidad
-//         return resultado
-//     } else {
-//         alert("El codigo ingresado del producto que buscas no existe")
-//     }
-// // }
-
-
-// // let formCompra = document.querySelector("#compra")
-// // formCompra.addEventListener("submit", function (e){
-// //     e.preventDefault()
-// //     comprar()
-// // })
